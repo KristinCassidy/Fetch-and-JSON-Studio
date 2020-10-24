5b78211107f8bdb94e7d8558c66d9fc7d15eae56
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-24 18:56:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.addEventListener("load", function() {
           <img class="avatar" src=${json[index].picture}>
         </div>
       `;
-        console.log(json);
+        console.log(json[2]);
       };
     });
   });
