7ffd8b9d4484ad8bf69ea0505705c97b66b5ce7b
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-24 19:12:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 window.addEventListener("load", function() {
-  //let json = [];
   fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
     response.json().then(function(json) {
-      const container = document.getElementById("container");
+      //const container = document.getElementById("container");
       for (let index = 0; index < json.length; index++){
-      container.innerHTML = `
-        <div class="astronaut">
-          <div class="bio">
-            <h3>${json[index].firstName} ${json[index].lastName}</h3>
-            <ul>
-              <li>Hours in Space: ${json[index].hoursInSpace}</li>
-              <li>Active: ${json[index].active}</li>
-              <li>Skills: ${json[index].skills}</li>
-            </ul>
-          </div>
-          <img class="avatar" src=${json[index].picture}>
-        </div>
-      `;
+        let astronautBio = document.createElement("div");
+          astronautBio.innerHTML = `
+            <div class="astronaut">
+              <div class="bio">
+                <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                <ul>
+                  <li>Hours in Space: ${json[index].hoursInSpace}</li>
+                  <li>Active: ${json[index].active}</li>
+                  <li>Skills: ${json[index].skills}</li>
+                </ul>
+              </div>
+              <img class="avatar" src=${json[index].picture}>
+            </div>
+          `;
+        document.getElementByID("container").appendChild(astronautBio);
         console.log(json[2]);
       };
     });
