e75e39587cf9747f0261d6c545e00df2bf0af879
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-26 03:12:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ window.addEventListener("load", function() {
                 <h3>${json[index].firstName} ${json[index].lastName}</h3>
                 <ul>
                   <li>Hours in Space: ${json[index].hoursInSpace}</li>
-                  <li id="activeStatus">Active: ${json[index].active}</li>
+                  <li id="activeStatus">Active: ${json[index].active;
+                     let trueStatus = document.getElementById("activeStatus");
+                        if (trueStatus.innerHTML == "Active: true") {
+                        trueStatus.style.color ="green";
+                        }}</li>
                   <li>Skills: ${json[index].skills.join(", ")}</li>
                 </ul>
               </div>
@@ -20,10 +24,10 @@ window.addEventListener("load", function() {
             </div>
           `;  
         document.getElementById("container").appendChild(astronautBio);
-        let trueStatus = document.getElementById("activeStatus");
-          if (trueStatus.innerHTML == "Active: true") {
-            trueStatus.style.color ="green";
-          }
+       // let trueStatus = document.getElementById("activeStatus");
+        //  if (trueStatus.innerHTML == "Active: true") {
+         //   trueStatus.style.color ="green";
+         // }
       };               
     });
   });
