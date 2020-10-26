c8ec7fe6463cb7eb3ccc513c523d402a5f54fb16
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-26 03:58:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function() {
       return a.hoursInSpace - b.hoursInSpace;
       });
       for (let index = 0; index < json.length; index++) {
-      let trueStatus = document.getElementById("activeStatus");
+      var trueStatus = document.getElementById("activeStatus");
           if (trueStatus.innerHTML == "Active: true") {
             trueStatus.style.color ="green";
           }};
